1b014ed6953e2c9721f3e8c354ccf01f7d4e87e4
Kevin Jung <kev.jung.ii@gmail.com>
2022-04-16 11:46:26 -0700

    Initial Commit

---
+++ Level Up/Level_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Level_UpApp.swift
+//  Level Up
+//
+//  Created by Kevin Jung on 4/16/22.
+//
+
+import SwiftUI
+
+@main
+struct Level_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
